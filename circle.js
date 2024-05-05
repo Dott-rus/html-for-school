a6d3c139743f55d0410995854990bebd4993039e
Dott-rus <dott.main@gmail.com>
2024-05-05 23:47:02 +0300

    new file:   avatar.jpg 	new file:   circle.css 	new file:   circle.js 	new file:   fonts.css 	new file:   github-logo.png 	new file:   index.html 	new file:   social-media.css 	new file:   styles.css 	new file:   vk-logo.png 	new file:   "\320\274\320\275\320\276\320\263\320\276\320\262\320\260\321\202\320\276_\320\262\321\200\320\265\320\274\320\265\320\275\320\270_\321\217_\320\277\321\200\320\276\321\201\320\270\320\264\320\265\320\273_\321\200\320\265\320\264\320\260\320\272\321\202\320\276\321\200\320\265.png"

---
+++ circle.js
@@ -0,0 +1,12 @@
+const blobs = document.getElementsByClassName("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  Array.from(blobs).forEach(blob => {
+    blob.animate({
+      left: `${clientX}px`,
+      top: `${clientY}px`
+    }, { duration: 3000, fill: "forwards" });
+  });
+}
